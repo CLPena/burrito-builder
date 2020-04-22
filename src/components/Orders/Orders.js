3eec9abb542dc08ed8b768fd74aa410e607f6830
Cristina Pena <cristinalpena@gmail.com>
2020-04-22 09:27:49 -0600

    Make App functional and Orders class component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Orders.css';
+import { connect } from 'react-redux';
+import { setOrders } from '../../actions';
+import { getOrders } from '../../apiCalls';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
-    return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
-    )
-  });
-
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+export class Orders extends Component {
+  constructor(props) {
+    super();
+  }
 }
 
-export default Orders;
+const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+const mapDispatchToProps = dispatch => ({
+    setOrders: (orders) => dispatch(setOrders(orders))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,45 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import { setOrders } from '../../actions';
-import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
-class App extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-  }
 
-  componentDidMount() {
-    getOrders()
-      .then(data => this.props.setOrders(data.orders))
-      .catch(err => console.error('Error fetching:', err));
-  }
+export const App = () => {
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
+      <Orders />
 
-        <Orders orders={this.props.orders}/>
-      </main>
-    );
-  }
+    </main>
+  );
 }
 
-const mapStateToProps = ({ orders }) => ({
-  orders
-});
+// const mapStateToProps = ({ orders }) => ({
+//   orders
+// });
+//
+// const mapDispatchToProps = dispatch => (
+//   {
+//     setOrders: (orders) => dispatch(setOrders(orders))
+//   }
+// );
 
-const mapDispatchToProps = dispatch => (
-  {
-    setOrders: (orders) => dispatch(setOrders(orders))
-  }
-);
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
